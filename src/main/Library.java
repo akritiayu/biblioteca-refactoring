5e28546523ed9e3619cd00cf3c7037f46a848979
Akriti Ayushi <akriti.ayushisingh@gmail.com>
2012-09-28 15:20:50 +0530

    My Second Commit

--- src/main/Library.java
+++ src/main/Library.java
@@ -4,120 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Library {
-    private static boolean loggedIn = false;
-    private static String savedLibraryNumber = "";
 
-    public static boolean performSelection(int option) {
-        if (option == 1) {
-            displayBookList();
-        } else if (option == 2) {
-            checkOutBook();       }
-        else if (option == 3) {
-            checkLibraryNumber();
-        } else if (option == 4) {
-            displayMovieList();
-        } else if (option == 5) {
-            login();
-
-        } else if (option == 9) {
-            System.out.println("Quitting...");
-            return true;
-        } else {
-            System.out.println("\n");
-            System.out.println("Enter a valid integer!!");
-        }
-        return false;
-    }
-
-    private static void login() {
-        clearLogin();
-        BufferedReader reader=createNewReader();
-        System.out.println("Enter your library number");
-        try {
-            String libraryNumber = reader.readLine();
-            if (validLibraryNumber(libraryNumber)) {
-                try {
-                    System.out.println("Enter your Password: ");
-                    String password = reader.readLine();
-                    if (validPassword(password)) {
-                        loggedIn = true;
-                        savedLibraryNumber = libraryNumber;
-                    }
-                } catch (Exception e) {
-
-                }
-            }
-        } catch (Exception e) {
-
-        }
-    }
-
-    private static void displayMovieList() {
-        System.out.println(createMovie("Rocky", "John G. Avildsen", "10"));
-        System.out.println(createMovie("Rocky II", "John G. Avildsen", "9"));
-        System.out.println(createMovie("Rocky III", "John G. Avildsen", "8"));
-        System.out.println(createMovie("Rocky IV", "John G. Avildsen", "7"));
-        System.out.println(createMovie("Rocky V", "John G. Avildsen", "8"));
-        System.out.println(createMovie("Drainage", "Francisco Trindade", "N/A"));
-        System.out.println(createMovie("The Shawshank Redemption", "Frank Darabont", "10"));
-        System.out.println(createMovie("The Godfather", "Francis Ford Coppola", "7"));
-        System.out.println(createMovie("Inception", "Frank Darabont", "10"));
-        System.out.println(createMovie("Pulp Fiction", "Quentin Tarantino", "6"));
-    }
-
-    private static void checkLibraryNumber() {
-        if (loggedIn()) {
-            System.out.println("\n");
-            System.out.println("Your library number is " + savedLibraryNumber);
-        } else {
-
-            System.out.println("\n");
-            System.out.println("Please talk to Librarian. Thank you.");
-        }
-    }
-
-    public static void checkOutBook() {
-
-        int option;
-        System.out.println(" Please enter the number of the book you wish to checkout: ");
-        option=0;
-        option = getSelctionFromUser();
-        if (option==1||option==2||option==3||option==4)
-        {
-
-            System.out.println("\n");
-            System.out.println(" Thank You! Enjoy the book.");
-
-
-        } else { System.out.println("\n");
-            System.out.println("Sorry we don't have that book yet.");
-        }
-    }
-
-    public static int getSelctionFromUser() {
-        BufferedReader reader=createNewReader();
-        int option = 0;
-        try {
-            String value = reader.readLine();
-            option = Integer.parseInt(value);
-        } catch (Exception e) {
-            // Do you know what numbers are!!!
-            System.out.println("Enter a valid integer!!");
-        }
-        return option;
-    }
-
-    private static BufferedReader createNewReader() {
-        InputStreamReader inputStream = new InputStreamReader(System.in);
-        return new BufferedReader(inputStream);
-    }
-
-    private static void displayBookList() {
-        System.out.println(" 1. Sweet Valley High vol. 4 by John Travolta ");
-        System.out.println(" 2. eXtreme Programming Explained by Kent Beck ");
-        System.out.println(" 3. How to Win Friends and Influence People by Dale Carnagie ");
-        System.out.println(" 4. How to Cheat at TWU Assignements by Anonymous ");
-    }
 
     public static void displayMenu() {
         System.out.println("**********************************************************");
@@ -133,28 +20,95 @@ public class Library {
         System.out.println("*         9. Exit                                        *");
         System.out.println("**********************************************************");
         System.out.println("Your Selection: ");
+
     }
 
-    private static boolean validPassword(String password) {
-        return "bhaisahab".equals(password);
+    public static int getSelctionFromUser() {
+        BufferedReader reader=createNewReader();
+        int option = 0;
+        try {
+            String value = reader.readLine();
+            option = Integer.parseInt(value);
+        } catch (Exception e) {
+            // Do you know what numbers are!!!
+            System.out.println("Enter a valid integer!!");
+        }
+        return option;
     }
 
-    private static boolean validLibraryNumber(String libraryNumber) {
-        return libraryNumber.matches("\\d\\d\\d-\\d\\d\\d\\d");
+    public static BufferedReader createNewReader() {
+        InputStreamReader inputStream = new InputStreamReader(System.in);
+        return new BufferedReader(inputStream);
     }
 
-    private static boolean loggedIn() {
-        return loggedIn;
+    public static boolean performSelection(int option) {
+        if (option == 1) {
+            displayBookList();
+        } else if (option == 2) {
+            checkOutBook();       }
+        else if (option == 3) {
+            checkLibraryNumber();
+        } else if (option == 4) {
+            displayMovieList();
+        } else if (option == 5) {
+            Login.login();
+
+        } else if (option == 9) {
+            System.out.println("Quitting...");
+            return true;
+        } else {
+            System.out.println("\n");
+            System.out.println("Enter a valid integer!!");
+        }
+        return false;
     }
 
+    private static void displayBookList() {
+        System.out.println(" 1. Sweet Valley High vol. 4 by John Travolta ");
+        System.out.println(" 2. eXtreme Programming Explained by Kent Beck ");
+        System.out.println(" 3. How to Win Friends and Influence People by Dale Carnagie ");
+        System.out.println(" 4. How to Cheat at TWU Assignements by Anonymous ");
+    }
 
-    private static void clearLogin() {
-        loggedIn = false;
-        savedLibraryNumber = "";
+    public static void checkOutBook() {
+        int option=0;
+        System.out.println(" Please enter the number of the book you wish to checkout: ");
+        option = getSelctionFromUser();
+        System.out.println("\n");
+        if (option==1||option==2||option==3||option==4)
+            System.out.println(" Thank You! Enjoy the book.");
+        else
+            System.out.println("Sorry we don't have that book yet.");
+    }
+
+    private static void checkLibraryNumber()
+    {
+        System.out.println("\n");
+        if (Login.loggedIn)
+            System.out.println("Your library number is " + Login.savedLibraryNumber);
+        else
+            System.out.println("Please talk to Librarian. Thank you.");
+    }
+
+    private static void displayMovieList() {
+        System.out.println(createMovie("Rocky", "John G. Avildsen", "10"));
+        System.out.println(createMovie("Rocky II", "John G. Avildsen", "9"));
+        System.out.println(createMovie("Rocky III", "John G. Avildsen", "8"));
+        System.out.println(createMovie("Rocky IV", "John G. Avildsen", "7"));
+        System.out.println(createMovie("Rocky V", "John G. Avildsen", "8"));
+        System.out.println(createMovie("Drainage", "Francisco Trindade", "N/A"));
+        System.out.println(createMovie("The Shawshank Redemption", "Frank Darabont", "10"));
+        System.out.println(createMovie("The Godfather", "Francis Ford Coppola", "7"));
+        System.out.println(createMovie("Inception", "Frank Darabont", "10"));
+        System.out.println(createMovie("Pulp Fiction", "Quentin Tarantino", "6"));
     }
 
     private static String createMovie(String movieTitle, String movieDirector, String movieRanking) {
         return movieTitle + " - Director: " + movieDirector + " Rating: " + movieRanking;
     }
+
+
+
+
 }
 
